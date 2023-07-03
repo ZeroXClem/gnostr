b0e67afe0071e86a212f4a97d1dd7297eeef4fb1
@RandyMcMillan <randy.lee.mcmillan@gmail.com>
2023-07-02 23:24:01 -0400

    make: ubuntu-jq

--- act.mk
+++ act.mk
@@ -4,10 +4,15 @@ ubuntu-git:submodules docker-start## 	run act in .github
 	#we use -b to bind the repo to the act container
 	#in the single dep instances we reuse (-r) the container
 	@type -P act && export $(cat ~/gh_token.txt) && act -vb -W $(PWD)/.github/workflows/$@.yml || $(MAKE) act-install
-ubuntu-jq:submodules docker-start## 	run act in .github
+.PHONY:deps/jq/.github/workflows/linux ubuntu-jq
+#ubuntu-jq:submodules docker-start jq/.github/workflows/linux## 	run act for deps/jq/.github/workflows/linux.yml
+ubuntu-jq:submodules docker-start## 	run act .github/workflows/ubuntu-jq.yml
+	#@pushd deps/jq && autoreconf -i && ./configure  --disable-maintainer-mode  && make all install
+	@type -P act && export ACTIONS_RUNTIME_TOKEN=$(cat ~/gh_token.txt) && act -vb -W $(PWD)/.github/workflows/$@.yml || $(MAKE) act-install
+jq/.github/workflows/linux:submodules docker-start## 	
 	#we use -b to bind the repo to the act container
 	#in the single dep instances we reuse (-r) the container
-	@type -P act && export $(cat ~/gh_token.txt) && act -vb -W $(PWD)/.github/workflows/$@.yml || $(MAKE) act-install
+	#@type -P act && export ACTIONS_RUNTIME_TOKEN=$(cat ~/gh_token.txt) && act -vb -C deps  -W $@.yml || $(MAKE) act-install
 ubuntu-nostcat:submodules docker-start## 	run act in .github
 	#we use -b to bind the repo to the act container
 	#in the single dep instances we reuse (-r) the container
