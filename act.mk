2e81d4474a7ab0de283eda438cdb4a126c99cb64
@RandyMcMillan <randy.lee.mcmillan@gmail.com>
2023-07-02 21:06:16 -0400

    make: act-install type -P act && ... || $(MAKE) act-install

--- act.mk
+++ act.mk
@@ -3,16 +3,15 @@ act-install:submodules## install act from deps/act/install.sh -b
 ubuntu-git:submodules docker-start## 	run act in .github
 	#we use -b to bind the repo to the act container
 	#in the single dep instances we reuse (-r) the container
-	@export $(cat ~/gh_token.txt) && act -v  -W $(PWD)/.github/workflows/$@.yml
+	@type -P act && export $(cat ~/gh_token.txt) && act -vb -W $(PWD)/.github/workflows/$@.yml || $(MAKE) act-install
 ubuntu-jq:submodules docker-start## 	run act in .github
 	#we use -b to bind the repo to the act container
 	#in the single dep instances we reuse (-r) the container
-	@export $(cat ~/gh_token.txt) && act -vb -W $(PWD)/.github/workflows/$@.yml
+	@type -P act && export $(cat ~/gh_token.txt) && act -vb -W $(PWD)/.github/workflows/$@.yml || $(MAKE) act-install
 ubuntu-nostcat:submodules docker-start## 	run act in .github
 	#we use -b to bind the repo to the act container
 	#in the single dep instances we reuse (-r) the container
-	@export $(cat ~/gh_token.txt) && act -v  -W $(PWD)/.github/workflows/$@.yml
-
+	@type -P act && export $(cat ~/gh_token.txt) && act -vb -W $(PWD)/.github/workflows/$@.yml || $(MAKE) act-install
 	#the matrix/pre/release builds are for the resulting app builds
 ubuntu-matrix:submodules docker-start## 	run act in .github
-	@export $(cat ~/gh_token.txt) && act -vb  -W $(PWD)/.github/workflows/$@.yml
+	@type -P act && export $(cat ~/gh_token.txt) && act -vb -W $(PWD)/.github/workflows/$@.yml || $(MAKE) act-install
