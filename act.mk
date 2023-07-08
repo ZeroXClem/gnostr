e6818592fa732d32fdd954e825360eb91c0fc97c
@RandyMcMillan <randy.lee.mcmillan@gmail.com>
2023-07-08 14:52:00 -0400

    act.mk: use @type -P act && GITHUB_TOKEN=$(c...

--- act.mk
+++ act.mk
@@ -4,20 +4,20 @@ act-install:## 	install act from deps/act/install.sh -b
 ubuntu-git:submodules docker-start## 	run act in .github
 	#we use -b to bind the repo to the act container
 	#in the single dep instances we reuse (-r) the container
-	@type -P act && GITHUB_TOKEN=$(cat ~/GITHUB_TOKEN.txt) && act -vb -W $(PWD)/.github/workflows/$@.yml || $(MAKE) act-install
+	@type -P act && GITHUB_TOKEN=$(cat ~/GITHUB_TOKEN.txt) && act $(VERBOSE) $(BIND) $(REUSE) -W $(PWD)/.github/workflows/$@.yml || $(MAKE) act-install
 .PHONY:deps/jq/.github/workflows/linux ubuntu-jq
 #ubuntu-jq:submodules docker-start jq/.github/workflows/linux## 	run act for deps/jq/.github/workflows/linux.yml
 ubuntu-jq:submodules docker-start## 	run act .github/workflows/ubuntu-jq.yml
 	##@pushd deps/jq && autoreconf -i && ./configure  --disable-maintainer-mode  && make all install
-	@type -P act && GITHUB_TOKEN=$(cat ~/GITHUB_TOKEN.txt) && act -vb -W $(PWD)/.github/workflows/$@.yml || $(MAKE) act-install
+	@type -P act && GITHUB_TOKEN=$(cat ~/GITHUB_TOKEN.txt) && act $(VERBOSE) $(BIND) $(REUSE) -W $(PWD)/.github/workflows/$@.yml || $(MAKE) act-install
 jq/.github/workflows/linux:submodules docker-start## 	
 	#we use -b to bind the repo to the act container
 	#in the single dep instances we reuse (-r) the container
-	@type -P act && GITHUB_TOKEN=$(cat ~/GITHUB_TOKEN.txt) && act -vb -W $(PWD)/.github/workflows/$@.yml || $(MAKE) act-install
+	@type -P act && GITHUB_TOKEN=$(cat ~/GITHUB_TOKEN.txt) && act $(VERBOSE) $(BIND) $(REUSE) -W $(PWD)/.github/workflows/$@.yml || $(MAKE) act-install
 ubuntu-nostcat:submodules docker-start## 	run act in .github
 	#we use -b to bind the repo to the act container
 	#in the single dep instances we reuse (-r) the container
-	@type -P act && GITHUB_TOKEN=$(cat ~/GITHUB_TOKEN.txt) && act -vb -W $(PWD)/.github/workflows/$@.yml || $(MAKE) act-install
+	@type -P act && GITHUB_TOKEN=$(cat ~/GITHUB_TOKEN.txt) && act $(VERBOSE) $(BIND) $(REUSE) -W $(PWD)/.github/workflows/$@.yml || $(MAKE) act-install
 	#the matrix/pre/release builds are for the resulting app builds
 ubuntu-matrix:submodules docker-start## 	run act in .github
-	@type -P act && GITHUB_TOKEN=$(cat ~/GITHUB_TOKEN.txt) && act -vb -W $(PWD)/.github/workflows/$@.yml || $(MAKE) act-install
+	@type -P act && GITHUB_TOKEN=$(cat ~/GITHUB_TOKEN.txt) && act $(VERBOSE) $(BIND) $(REUSE) -W $(PWD)/.github/workflows/$@.yml || $(MAKE) act-install
