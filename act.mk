11022e7ee7c4427b2326625cbf98d4bc248affa8
@RandyMcMillan <randy.lee.mcmillan@gmail.com>
2023-07-08 12:35:54 -0400

    act.mk: use GITHUB_TOKEN in make ubuntu-matrix

--- act.mk
+++ act.mk
@@ -1,22 +1,23 @@
-act-install:submodules## 	install act from deps/act/install.sh -b
+act-install:## 	install act from deps/act/install.sh -b
+	@git submodule update --init --recursive  deps/act
 	./deps/act/install.sh -b /usr/local/bin && exec bash
 ubuntu-git:submodules docker-start## 	run act in .github
 	#we use -b to bind the repo to the act container
 	#in the single dep instances we reuse (-r) the container
-	@type -P act && export $(cat ~/gh_token.txt) && act -vb -W $(PWD)/.github/workflows/$@.yml || $(MAKE) act-install
+	@type -P act && GITHUB_TOKEN=$(cat ~/GITHUB_TOKEN.txt) && act -vb -W $(PWD)/.github/workflows/$@.yml || $(MAKE) act-install
 .PHONY:deps/jq/.github/workflows/linux ubuntu-jq
 #ubuntu-jq:submodules docker-start jq/.github/workflows/linux## 	run act for deps/jq/.github/workflows/linux.yml
 ubuntu-jq:submodules docker-start## 	run act .github/workflows/ubuntu-jq.yml
 	##@pushd deps/jq && autoreconf -i && ./configure  --disable-maintainer-mode  && make all install
-	@type -P act && export ACTIONS_RUNTIME_TOKEN=$(cat ~/gh_token.txt) && act -vb -W $(PWD)/.github/workflows/$@.yml || $(MAKE) act-install
+	@type -P act && GITHUB_TOKEN=$(cat ~/GITHUB_TOKEN.txt) && act -vb -W $(PWD)/.github/workflows/$@.yml || $(MAKE) act-install
 jq/.github/workflows/linux:submodules docker-start## 	
 	#we use -b to bind the repo to the act container
 	#in the single dep instances we reuse (-r) the container
-	#@type -P act && export ACTIONS_RUNTIME_TOKEN=$(cat ~/gh_token.txt) && act -vb -C deps  -W $@.yml || $(MAKE) act-install
+	@type -P act && GITHUB_TOKEN=$(cat ~/GITHUB_TOKEN.txt) && act -vb -W $(PWD)/.github/workflows/$@.yml || $(MAKE) act-install
 ubuntu-nostcat:submodules docker-start## 	run act in .github
 	#we use -b to bind the repo to the act container
 	#in the single dep instances we reuse (-r) the container
-	@type -P act && export $(cat ~/gh_token.txt) && act -vb -W $(PWD)/.github/workflows/$@.yml || $(MAKE) act-install
+	@type -P act && GITHUB_TOKEN=$(cat ~/GITHUB_TOKEN.txt) && act -vb -W $(PWD)/.github/workflows/$@.yml || $(MAKE) act-install
 	#the matrix/pre/release builds are for the resulting app builds
 ubuntu-matrix:submodules docker-start## 	run act in .github
-	@type -P act && export $(cat ~/gh_token.txt) && act -vb -W $(PWD)/.github/workflows/$@.yml || $(MAKE) act-install
+	@type -P act && GITHUB_TOKEN=$(cat ~/GITHUB_TOKEN.txt) && act -vb -W $(PWD)/.github/workflows/$@.yml || $(MAKE) act-install
