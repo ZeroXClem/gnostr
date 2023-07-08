322a8dadd97ca91cb738ec53f994a54241f25b5d
@RandyMcMillan <randy.lee.mcmillan@gmail.com>
2023-07-07 22:32:12 -0400

    act.mk:

--- act.mk
+++ act.mk
@@ -1,4 +1,4 @@
-act-install:submodules## install act from deps/act/install.sh -b
+act-install:submodules## 	install act from deps/act/install.sh -b
 	./deps/act/install.sh -b /usr/local/bin && exec bash
 ubuntu-git:submodules docker-start## 	run act in .github
 	#we use -b to bind the repo to the act container
@@ -7,7 +7,7 @@ ubuntu-git:submodules docker-start## 	run act in .github
 .PHONY:deps/jq/.github/workflows/linux ubuntu-jq
 #ubuntu-jq:submodules docker-start jq/.github/workflows/linux## 	run act for deps/jq/.github/workflows/linux.yml
 ubuntu-jq:submodules docker-start## 	run act .github/workflows/ubuntu-jq.yml
-	#@pushd deps/jq && autoreconf -i && ./configure  --disable-maintainer-mode  && make all install
+	##@pushd deps/jq && autoreconf -i && ./configure  --disable-maintainer-mode  && make all install
 	@type -P act && export ACTIONS_RUNTIME_TOKEN=$(cat ~/gh_token.txt) && act -vb -W $(PWD)/.github/workflows/$@.yml || $(MAKE) act-install
 jq/.github/workflows/linux:submodules docker-start## 	
 	#we use -b to bind the repo to the act container
