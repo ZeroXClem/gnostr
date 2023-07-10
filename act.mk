ee674db36fba0ded37fefc29e7fc101dbdbff5d2
@RandyMcMillan <randy.lee.mcmillan@gmail.com>
2023-07-10 16:50:19 -0400

    make: ubuntu-gnostr-cat

--- act.mk
+++ act.mk
@@ -14,7 +14,7 @@ jq/.github/workflows/linux:submodules docker-start##
 	#we use -b to bind the repo to the act container
 	#in the single dep instances we reuse (-r) the container
 	@type -P act && GITHUB_TOKEN=$(shell cat ~/GITHUB_TOKEN.txt) && act $(VERBOSE) $(BIND) $(REUSE) -W $(PWD)/.github/workflows/$@.yml || $(MAKE) act-install
-ubuntu-nostcat:submodules docker-start## 	run act in .github
+ubuntu-gnostr-cat:submodules docker-start## 	run act in .github
 	#we use -b to bind the repo to the act container
 	#in the single dep instances we reuse (-r) the container
 	type -P act && GITHUB_TOKEN=$(shell cat ~/GITHUB_TOKEN.txt) && act $(VERBOSE) $(BIND) $(REUSE) -W $(PWD)/.github/workflows/$@.yml || $(MAKE) act-install
