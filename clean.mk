59910764f1dd2c23d34ea76ecb7f647bbe640edc
@RandyMcMillan <randy.lee.mcmillan@gmail.com>
2023-07-12 17:35:14 -0400

    make: clean: clean-secp: fix

--- clean.mk
+++ clean.mk
@@ -1,53 +1,53 @@
 ##clean
 ##	remove gnostr *.o *.a gnostr.1
 clean:## 	remove gnostr *.o *.a gnostr.1
-	rm -rf $(shell which gnostr)
-	rm -rf /usr/local/share/man/man1/gnostr.1
-	rm -f gnostr *.o *.a
+	rm -rf $(shell which gnostr) || echo
+	rm -rf /usr/local/share/man/man1/gnostr.1 || echo
+	rm -f gnostr *.o *.a || echo
 
 ##clean-hyper-nostr
 ##	remove deps/hyper-nostr
 clean-hyper-nostr:## 	remove deps/hyper-nostr
-	rm -rf deps/hyper-nostr
+	rm -rf deps/hyper-nostr || echo
 
 ##clean-hyper-sdk
 ##	remove deps/hypersdk
 clean-hyper-sdk:## 	remove deps/hyper-sdk
-	rm -rf deps/hyper-sdk
+	rm -rf deps/hyper-sdk || echo
 
 ##clean-secp
 ##	remove deps/secp256k1/.libs/libsecp256k1.*
 clean-secp:## 	remove deps/secp256k1/.libs/libsecp256k1.* libsecp256k1.a
-	rm -rf deps/secp256k1/.libs/libsecp256k1.*
-	rm libsecp256k1.a
+	rm -rf deps/secp256k1 || echo
+	rm -rf libsecp256k1.a || echo
 
 ##clean-gnostr-git
 ##	remove deps/gnostr-git/gnostr-git
 ##	remove gnostr-git
 clean-gnostr-git:## 	remove deps/gnostr-git gnostr-git
 	#rm -rf deps/gnostr-git
-	rm deps/gnostr-git/gnostr-git
-	rm gnostr-git
+	rm deps/gnostr-git/gnostr-git || echo
+	rm gnostr-git || echo
 
 ##clean-gnostr-cat
 ##	remove deps/gnostr-cat
 clean-gnostr-cat:## 	remove deps/gnostr-cat
-	rm -rf deps/gnostr-cat
+	rm -rf deps/gnostr-cat || echo
 
 ##clean-gnostr-relay
 ##	remove deps/gnostr-relay
 clean-gnostr-relay:## 	remove deps/gnostr-relay
-	rm -rf deps/gnostr-relay
+	rm -rf deps/gnostr-relay || echo
 
 ##clean-tcl
 ##	remove deps/tcl
 clean-tcl:## 	remove deps/tcl
-	rm -rf deps/tcl
+	rm -rf deps/tcl || echo
 
 ##clean-jq
 ##	remove deps/jq
 clean-jq:## 	remove deps/jq
-	rm -rf deps/jq
+	rm -rf deps/jq || echo
 clean-all:clean clean-hyper-nostr clean-secp clean-gnostr-git clean-tcl clean-jq## 	clean clean-*
 ##clean-all
 ##	clean clean-hyper-nostr clean-secp clean-gnostr-git clean-tcl clean-jq
