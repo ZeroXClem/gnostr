1fd70eb96e1586508bf5b6d85d435f47bca4ec7f
randymcmillan <randy.lee.mcmillan@gmail.com>
2023-09-28 20:48:40 -0400

    gnostr.js:spelling correction

--- gnostr.js
+++ gnostr.js
@@ -9,19 +9,19 @@ if (!shell.which('git')) {
 }
 // console.log(shell.which('git'));
 if (!shell.which('gnostr')) {
-  console.log(shel.which('gnostr'));
+  console.log(shell.which('gnostr'));
   shell.echo('Sorry, this script requires gnostr');
   shell.exit(1);
 }
 // console.log(shell.which('gnostr'));
 if (!shell.which('gnostr-proxy')) {
-  console.log(shel.which('gnostr-proxy'));
+  console.log(shell.which('gnostr-proxy'));
   shell.echo('Sorry, this script requires gnostr');
   shell.exit(1);
 }
 // console.log(shell.which('gnostr-proxy'));
 if (!shell.which('gnostr-sha256')) {
-  console.log(shel.which('gnostr-sha256'));
+  console.log(shell.which('gnostr-sha256'));
   shell.echo('Sorry, this script requires gnostr-sha256');
   shell.exit(1);
 }
