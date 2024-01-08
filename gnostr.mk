9f6732456fa4312b8245434bec57891cebc5c3fe
@RandyMcMillan <randy.lee.mcmillan@gmail.com>
2024-01-07 20:10:54 -0500

    gnostr.mk:bins-test-post-event:bins-test-fetch-by-id

--- gnostr.mk
+++ gnostr.mk
@@ -274,6 +274,10 @@ gnostr-command:deps/gnostr-command/target/release/gnostr-command## 	gnostr-comma
 .PHONY:bins
 bins:
 	@cd bins && make cargo-b-release && make cargo-i
+bins-test-post-event:
+	cat test/first-gnostr-commit.json | gnostr-post-event wss://relay.damus.io
+bins-test-fetch-by-id:
+		gnostr-fetch-by-id wss://relay.damus.io fbf73a17a4e0fe390aba1808a8d55f1b50717d5dd765b2904bf39eba18c51f7c
 
 .PHONY:tui
 tui:
